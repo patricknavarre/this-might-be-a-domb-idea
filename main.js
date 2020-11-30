8ee456a29cb168dae95dbdcbf2d520e97f94ff95
patricknavarre <patrick.navarre@codeimmersives.com>
2020-11-30 14:50:35 -0500

    almost-finished

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-1
+const changeParaColor = document.querySelector('p')
+changeParaColor.style.color = 'lightblue';
+
+const changeHeader = document.querySelector('h1')
+changeHeader.style.fontSize = '5em';
+
+const makeHipster = document.querySelector('p')
+makeHipster.innerHTML = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+const halfTransparent = document.querySelector('#item-13')
+halfTransparent.style.opacity = '0.5';
+
+const changeThirdItem = document.querySelector('#item-3')
+changeThirdItem.innerHTML = 'I say, "Hi!"'
+
+const addDinoPic = document.querySelector('img')
+addDinoPic.src = ('http://www.tioxic.com/wp-content/uploads/trex_4.jpg')
+addDinoPic.height = '300'
+
+const changePicSize = document.querySelector('#api-pic')
+changePicSize.height = '300'
+
+const addLi = document.querySelector('#item-16')
+addLi.innerText = 'wont get fooled again';
+
+
+
+
